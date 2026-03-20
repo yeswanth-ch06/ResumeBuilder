a3248cf4c2b7db7bd82d52f3a3b89894f0891c44
yeswanth <yeswanthchandaka3@gmail.com>
2026-03-20 13:33:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,42 @@ def homepage():
 @app.route("/resume",methods = ["GET","POST"])
 def resume():
         if request.method == "POST":
+            exp = []
+            expdesc = []
+            intern = []
+            interndesc = []
+
+            proj = []
+            projdesc = []
+            cert = []
+            certdesc = []
+
+            for i in range(1,5):
+                if request.form.get(f"exp{i}"):
+                    exp.append(request.form.get(f"exp{i}"))
+                if request.form.get(f"expdesc{i}"):
+                    expdesc.append(request.form.get(f"expdesc{i}"))
+                if request.form.get(f"intern{i}"):
+                    intern.append(request.form.get(f"intern{i}"))
+                if request.form.get(f"interndesc{i}"):
+                    interndesc.append(request.form.get(f"interndesc{i}"))
+                if request.form.get(f"proj{i}"):
+                    proj.append(request.form.get(f"proj{i}"))
+                if request.form.get(f"projdesc{i}"):
+                    projdesc.append(request.form.get(f"projdesc{i}"))
+
+            for i in range(1,7):
+                if request.form.get(f"cert{i}"):
+                    cert.append(request.form.get(f"cert{i}"))
+                if request.form.get(f"certdesc{i}"):
+                    certdesc.append(request.form.get(f"certdesc{i}"))
             photo = request.files.get("photo")
             if photo:
                 photo_path = photo.filename
                 photo.save("static/"+ photo_path)
             else:
                 photo_path = ""
+
             data = {
                 "photo" : photo_path,
                 "name" : request.form.get("name"),
@@ -36,16 +66,13 @@ def resume():
                 "branch" : request.form.get("branch"),
                 "grdyear" : request.form.get("grdyear"),
                 "cgpa" : request.form.get("cgpa"),
-                "techskills": request.form.get("techskills", "").split("\n"),
-                "softskills": request.form.get("softskills", "").split("\n"),
                 "lang": request.form.get("lang", "").split("\n"),
-                "exp": request.form.get("exp", "").split("\n"),
-                "intern": request.form.get("intern", "").split("\n"),
                 "desc" : request.form.get("desc"),
-                "projects" : request.form.get("proj","").split("\n"),
-                "certificates" : request.form.get("cert","").split("\n")
-            }
-            return render_template("resume.html",data = data)
+                "progskills": request.form.get("progskills", "").split("\n"),
+                "dbskills": request.form.get("dbskills", "").split("\n"),
+                "tools": request.form.get("tools", "").split("\n"),
+            }                
+            return render_template("resume.html",data = data,exps = zip(exp,expdesc), intern = zip(intern,interndesc), projs = zip(proj,projdesc), certs = zip(cert,certdesc))
         return redirect(url_for("homepage"))
 
 # if __name__ == "__main__":
