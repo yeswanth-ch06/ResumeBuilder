8eda00bcde209f4235d704a95682e47b295333bb
yeswanth <yeswanthchandaka3@gmail.com>
2026-04-05 18:30:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,11 +58,13 @@ def resume():
                 "progskills": request.form.get("progskills", "").split("\n"),
                 "dbskills": request.form.get("dbskills", "").split("\n"),
                 "tools": request.form.get("tools", "").split("\n"),
-            }                
-            if layout = "three":
-                return render_template("three_resume.html",data = data,exps = list(zip(exp,expdesc)), projs = list(zip(proj,projdesc)), certs = list(zip(cert,certdesc)))
-            else:
-                return render_template("one_resume.html",data = data,exps = list(zip(exp,expdesc)), projs = list(zip(proj,projdesc)), certs = list(zip(cert,certdesc)))
+            }           
+
+            return render_template("three_resume.html",data = data,exps = list(zip(exp,expdesc)), projs = list(zip(proj,projdesc)), certs = list(zip(cert,certdesc)))
+            # if layout = "three":
+            #     return render_template("three_resume.html",data = data,exps = list(zip(exp,expdesc)), projs = list(zip(proj,projdesc)), certs = list(zip(cert,certdesc)))
+            # else:
+            #     return render_template("one_resume.html",data = data,exps = list(zip(exp,expdesc)), projs = list(zip(proj,projdesc)), certs = list(zip(cert,certdesc)))
             
         return redirect(url_for("homepage"))
 
